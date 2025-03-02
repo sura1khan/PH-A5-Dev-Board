14a76b61824e9615467fd6f6ec78d0b8a0c0cd39
Suraia <khansuraia645@gmail.com>
2025-03-02 16:02:12 +0600

    task checekbox

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
-// let today = new Date();
-// console.log(today)
-
-// document.getElementById("date")
-// .value = new Date().toISOString().split("T")[0];
+//date
 function updateDate() {
     document.getElementById("Date").innerText = new Date().toDateString();
   }
   setInterval(updateDate, 86400000);
   updateDate(); 
+
+ //completing tasks
+function completeTask(button) {
+    let taskItem = button.parentElement; 
+    taskItem.remove();
+    let pendingCountElement = document.getElementById("count");
+    let pendingCount = parseInt(pendingCountElement.innerText);
+    if (pendingCount > 0) {
+        pendingCountElement.innerText = pendingCount - 1;
+    } 
+    let completedCountElement = document.getElementById("check");
+    let completedCount = parseInt(completedCountElement.innerText);
+    completedCountElement.innerText = completedCount + 1;
+}
