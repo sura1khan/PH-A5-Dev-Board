25d5073e5274e66d23df40fe2e517ad5e84ad602
Suraia <khansuraia645@gmail.com>
2025-03-02 23:22:54 +0600

    colortheme button

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+function changeBackgroundColor() {
+    document.body.style.backgroundColor = getRandomColor();
+}
 
 function updateDate() {
     let dateElements = document.getElementsByClassName("Date");
